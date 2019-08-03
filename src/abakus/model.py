a8b4b35a2867539b83f923b56194d29891f46cb8
cb <9629717+hansi-b@users.noreply.github.com>
2019-08-03 10:30:37 +0200

    added minimal kosten-Functions to ÖtvKosten class

--- src/abakus/model.py
+++ src/abakus/model.py
@@ -8,10 +8,10 @@ from decimal import Decimal
 from typing import Mapping, Tuple
 from enum import Enum
 
-# (Gruppe), Stufe x Jahr -> (Brutto, Jahressonderzahlung)
+# (Entgeltgruppe), Stufe x Jahr -> (Brutto, Jahressonderzahlung)
 
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 class Gehalt:
     """
         Monatliche Gehaltsdaten: Bruttogehalt (ohne Arbeitgeberzuschlag) und Sonderzahlung.
@@ -22,6 +22,7 @@ class Gehalt:
 
 class Stufe(Enum):
     """
+        definiert, mit welchen Stufen wir arbeiten können;
         eigentlich nur ein Typwrapper für die ganzzahlige Stufe
     """
     eins = 1
@@ -32,24 +33,54 @@ class Stufe(Enum):
     sechs = 6
 
 
-@dataclass
-class Entgeltgruppe:
+class Entgeltgruppe(Enum):
     """
-        die Zuordnung von Stufen zu Gehältern
+        Definition der Entgeltgruppen, für die Gehaltsdaten vorliegen
     """
-    stufen : Mapping [ Stufe, Gehalt ]
+    E_10 = 10
+    E_13 = 13
 
 
-@dataclass
-class ÖTV:
+@dataclass(eq=True, frozen=True)
+class GuS:
     """
-        Der Tarif ist eine Festlegung von Gehältern pro Gruppe und Jahr
+        Kurz für Entgeltgruppe und Stufe
+        Klein, aber wir werden ständig mit dieser Kombination arbeiten
     """
-    gehälter : Mapping [ Tuple[int, str], Entgeltgruppe ]
+    gruppe : Entgeltgruppe
+    stufe : Stufe
+
+
+class ÖtvKosten:
+
+    """
+    ein fixer Prozentsatz, der als Kostenzuschlag genommen wird
+    """
+    arbeitgeberKostenZuschlag = 0.3
+
+    gehälter : Mapping [ Tuple[int, GuS], Gehalt ]
+    
+    def __init__(self, gehälter):
+        self.gehälter = gehälter
+
+    def summeMonatlich(self, jahr: int, gus : GuS):
+        """
+            :return: die monatlichen Gesamtkosten mit Arbeitgeberzuschlag,
+                    aber ohne Jahressonderzahlung
+        """
+        return self.gehälter[(jahr, gus)].brutto * (1. + ÖtvKosten.arbeitgeberKostenZuschlag)
+
+    def sonderzahlung(self, jahr: int, gus : GuS):
+        """
+            :return: die Jahressonderzahlung
+        """
+        return self.gehälter[(jahr, gus)].sonderzahlung
+    
+    def summeMonatlichMitSonderzahlung(self, jahr: int, gus : GuS):
+        
+        return self.summeMonatlich(jahr, gus) + self.sonderzahlung(jahr, gus)
 
 
 if __name__ == '__main__':
-    ö = ÖTV({})
-    ö.gehälter[(123, "hello")] = Entgeltgruppe({})
-    print(ö)
+    ö = ÖtvKosten({})
 
--- test/tests/abakus/modelTest.py
+++ test/tests/abakus/modelTest.py
@@ -1,13 +1,34 @@
 import unittest
-from abakus.model import Gehalt
-
-class GehälterTest(unittest.TestCase):
-
-    def testStufen(self):
-        g = Gehalt(2400, 200)
-        print(g)
+from abakus.model import Gehalt, GuS, Entgeltgruppe, \
+    Stufe, ÖtvKosten
 
 
+class KostenBerechnungTest(unittest.TestCase):
+
+    def setUp(self):
+        self.gehälter = {}
+
+    def tearDown(self):
+        self.gehälter = {}
+    
+    def testSummeMonatlich(self):
+        self.givenGehalt(2012, GuS(Entgeltgruppe.E_10, Stufe.drei),  #
+                         Gehalt(4., 7.))
+        ötv = ÖtvKosten(self.gehälter)
+        self.assertEquals(4. * 1.3,
+                          ötv.summeMonatlich(2012, GuS(Entgeltgruppe.E_10, Stufe.drei)))
+
+    def testSonderzahlung(self):
+        self.givenGehalt(2012, GuS(Entgeltgruppe.E_10, Stufe.drei),  #
+                         Gehalt(4., 7.2))
+        ötv = ÖtvKosten(self.gehälter)
+        
+        self.assertEquals(7.2,
+                          ötv.sonderzahlung(2012, GuS(Entgeltgruppe.E_10, Stufe.drei)))
+
+    def givenGehalt(self, jahr : int, gus : GuS, gehalt: Gehalt):
+        self.gehälter[(jahr, gus)] = gehalt
+        
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
