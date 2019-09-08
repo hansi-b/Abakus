44bfcdedf309fcca63d6a8ee628a7849838e7178
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2019-09-08 13:26:00 +0200

    test refactorings
    * moved laufendTest out of gui package
    * replaced deprecated assertEquals with assertEqual

--- test/tests/abakus/modelTest.py
+++ test/tests/abakus/modelTest.py
@@ -8,40 +8,40 @@ import decimal
 class StufenTest(unittest.TestCase):
     
     def testNächsterAufstieg(self):
-        self.assertEquals(date(2020, 3, 12),
+        self.assertEqual(date(2020, 3, 12),
             Stufe.eins.nächsterAufstieg(date(2019, 3, 12)))  # @UndefinedVariable
-        self.assertEquals(date(2022, 12, 22),
+        self.assertEqual(date(2022, 12, 22),
             Stufe.drei.nächsterAufstieg(date(2019, 12, 22)))  # @UndefinedVariable
 
     def testNächste(self):
-        self.assertEquals(Stufe.eins.nächste(), Stufe.zwei)  # @UndefinedVariable
-        self.assertEquals(Stufe.sechs.nächste(), Stufe.sechs)  # @UndefinedVariable
+        self.assertEqual(Stufe.eins.nächste(), Stufe.zwei)  # @UndefinedVariable
+        self.assertEqual(Stufe.sechs.nächste(), Stufe.sechs)  # @UndefinedVariable
 
 
 class DatierteGuSTest(unittest.TestCase):
 
     def testAmEinfach(self):
         start = Stelle(AllGuS.E10_2, date(2019, 1, 1))
-        self.assertEquals(start, start.am(date(2019, 2, 1)))
+        self.assertEqual(start, start.am(date(2019, 2, 1)))
 
     def testAmAufstieg(self):
         start = Stelle(AllGuS.E10_1, date(2019, 1, 1))
-        self.assertEquals(Stelle(AllGuS.E10_2, date(2020, 1, 1)),
+        self.assertEqual(Stelle(AllGuS.E10_2, date(2020, 1, 1)),
                           start.am(date(2020, 2, 17)))
 
     def testAmRandfall(self):
         start = Stelle(AllGuS.E10_1, date(2019, 1, 1))
-        self.assertEquals(Stelle(AllGuS.E10_2, date(2020, 1, 1)),
+        self.assertEqual(Stelle(AllGuS.E10_2, date(2020, 1, 1)),
                           start.am(date(2020, 1, 1)))
 
     def testAmÜbersprung(self):
         start = Stelle(AllGuS.E10_1, date(2019, 1, 1))
-        self.assertEquals(Stelle(AllGuS.E10_3, date(2022, 1, 1)),
+        self.assertEqual(Stelle(AllGuS.E10_3, date(2022, 1, 1)),
                           start.am(date(2023, 3, 4)))
 
     def testAmEndstufe(self):
         start = Stelle(AllGuS.E10_6, date(2019, 1, 1))
-        self.assertEquals(start, start.am(date(2043, 3, 4)))
+        self.assertEqual(start, start.am(date(2043, 3, 4)))
 
 
 class KostenBerechnungTest(unittest.TestCase):
@@ -62,7 +62,7 @@ class KostenBerechnungTest(unittest.TestCase):
         self.givenGehalt(2012, AllGuS.E10_3, Gehalt(4., 7.2))
         ötv = ÖtvKosten(self.gehälter)
         
-        self.assertEquals(7.2,
+        self.assertEqual(7.2,
                           ötv.sonderzahlung(2012, AllGuS.E10_3))
 
     def givenGehalt(self, jahr : int, gus : GuS, gehalt: Gehalt):
